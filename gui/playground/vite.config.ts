fcd97005f4e9b301915a63aa8d374c22b8699a24
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-04-20 18:57:47 +0200

    refactor: update vite.config.ts to define public directory path

--- gui/playground/vite.config.ts
+++ gui/playground/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  publicDir: resolve(__dirname, '../public'),
   server: {
     proxy: {
       // Proxy specific API routes to your Axum server
