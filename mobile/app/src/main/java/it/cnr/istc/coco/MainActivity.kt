d9c42e0405d2912f11961f177bb68b78a73986b0
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-05-21 14:32:26 +0200

    feat: initialize Android project structure with Gradle configuration and main components

---
+++ mobile/app/src/main/java/it/cnr/istc/coco/MainActivity.kt
@@ -0,0 +1,11 @@
+package it.cnr.istc.coco
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ mobile/app/src/main/java/it/cnr/istc/coco/MessagingService.kt
@@ -0,0 +1,17 @@
+package it.cnr.istc.coco
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("MessagingService", "New token: $token")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+    }
+}
