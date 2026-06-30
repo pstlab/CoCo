583f361f76e073891d765b9c9874feda810e1c24
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-06-30 11:05:37 +0200

    minor cleanings..

--- api/web/playground/main.ts
+++
@@ -1,8 +0,0 @@
-import { flick } from '@ratiosolver/flick';
-import { coco } from '../src/coco';
-import { CoCoApp } from '../src/components/app';
-import '@fortawesome/fontawesome-free/css/all.css';
-
-const cc = new coco.CoCo();
-
-flick.mount(() => CoCoApp(cc));
