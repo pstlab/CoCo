f81734a97cb58fc42b8dca011490393b959b9a05
riccardodebenedictis <riccardo.debenedictis@cnr.it>
2026-07-04 11:17:49 +0200

    add WiFi connection and WebSocket client functionality

---
+++ api/micropython/coco.py
@@ -0,0 +1,13 @@
+import machine
+import network
+
+
+def connect_wifi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print("Connecting to WiFi...")
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            machine.idle()
+    print("Connected to WiFi:", wlan.ifconfig())
